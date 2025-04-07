b09c31e330ccadd179eaa3cae50ada070bc5b906
Emily Vaz <89800341+sunpills@users.noreply.github.com>
2025-04-07 14:48:50 -0300

    corrigindo condicional

--- astro-quest/Controller/ViewController.swift
+++ astro-quest/Controller/ViewController.swift
@@ -17,23 +17,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var falseButton: UIButton!
     
-    var questionNumber = 0
-    
-    let quiz = [
-        Question(q: "O sol é uma estrela", a: "True"),
-        Question(q: "Os dias são mais curtos em Marte", a: "False"),
-        Question(q: "O sistema estelar mais próximo da Terra fica a 4,3 anos-luz", a: "True"),
-        Question(q: "A Lua não tem um lado escuro, todos os lados recebem luz do Sol em algum momento", a: "True"),
-        Question(q: "Saturno é o único planeta do Sistema Solar que tem anéis", a: "True"),
-        Question(q: "Se olharmos para galáxias distantes estamos literalmente olhando para o passado do universo", a: "True"),
-        Question(q: "O primeiro satélite artificial da Terra foi lançado pelos Estados Unidos", a: "False"),
-        Question(q: "A temperatura média no espaço sideral é de aproximadamente -270°C", a: "True"),
-        Question(q: "Plutão foi rebaixado a planeta anão SÓ porque é menor que a Lua da Terra", a: "False"),
-        Question(q: "O dia em Vênus é mais curto que seu ano", a: "False"),
-        Question(q: "Se o Sol desaparecesse de repente, a Terra continuaria orbitando por cerca de 8 minutos antes de sair da trajetória", a: "True"),
-        Question(q: "A maioria das estrelas visíveis no céu noturno já está morta devido ao tempo que a luz leva para chegar até nós", a: "False"),
-        Question(q: "A Via Láctea e a Galáxia de Andrômeda estão em rota de colisão e irão se fundir um dia", a: "True")
-    ]
+    var quizQuestions = QuizQuestions()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,11 +27,10 @@ class ViewController: UIViewController {
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
-        let userAnswer = sender.currentTitle
-        let actualQuestion = quiz[questionNumber]
-        let actualAnswer = actualQuestion.answer
+        let userAnswer = sender.currentTitle!
+        let rightAnswer = quizQuestions.checkAnswer(userAnswer)
         
-        if userAnswer == actualAnswer {
+        if rightAnswer {
             sender.backgroundColor = UIColor.green
         } else {
             sender.backgroundColor = UIColor.red
@@ -64,10 +47,11 @@ class ViewController: UIViewController {
     }
     
     @objc func updateUI() {
-        questionLabel.text = quiz[questionNumber].text
+        questionLabel.text = quizQuestions.getQuestionText()
+        progressBar.progress = quizQuestions.getProgress()
+        
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
-        progressBar.progress = Float (questionNumber + 1) / Float (quiz.count)
     }
     
     
--- astro-quest/Model/QuizQuestions.swift
+++ astro-quest/Model/QuizQuestions.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+struct QuizQuestions {
+    
+    let quiz = [
+        Question(q: "O sol é uma estrela", a: "True"),
+        Question(q: "Os dias são mais curtos em Marte", a: "False"),
+        Question(q: "O sistema estelar mais próximo da Terra fica a 4,3 anos-luz", a: "True"),
+        Question(q: "A Lua não tem um lado escuro, todos os lados recebem luz do Sol em algum momento", a: "True"),
+        Question(q: "Saturno é o único planeta do Sistema Solar que tem anéis", a: "True"),
+        Question(q: "Se olharmos para galáxias distantes estamos literalmente olhando para o passado do universo", a: "True"),
+        Question(q: "O primeiro satélite artificial da Terra foi lançado pelos Estados Unidos", a: "False"),
+        Question(q: "A temperatura média no espaço sideral é de aproximadamente -270°C", a: "True"),
+        Question(q: "Plutão foi rebaixado a planeta anão SÓ porque é menor que a Lua da Terra", a: "False"),
+        Question(q: "O dia em Vênus é mais curto que seu ano", a: "False"),
+        Question(q: "Se o Sol desaparecesse de repente, a Terra continuaria orbitando por cerca de 8 minutos antes de sair da trajetória", a: "True"),
+        Question(q: "A maioria das estrelas visíveis no céu noturno já está morta devido ao tempo que a luz leva para chegar até nós", a: "False"),
+        Question(q: "A Via Láctea e a Galáxia de Andrômeda estão em rota de colisão e irão se fundir um dia", a: "True")
+    ]
+    
+    var questionNumber = 0
+    
+    func checkAnswer(_ userAnswer: String) -> Bool {
+        if userAnswer == quiz[questionNumber].answer {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func getQuestionText() -> String {
+        return quiz[questionNumber].text
+    }
+    
+    func getProgress() -> Float {
+        let progress = Float (questionNumber) / Float (quiz.count)
+        return progress
+    }
+}
